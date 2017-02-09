068b38c85f56cdc0c00a45ba15c6251829cdf976
Tobotimus <tobyharradine@live.com>
2017-02-09 21:34:43 +1100

    Better support for removing roles which have been removed from server.

--- cogs/register.py
+++ cogs/register.py
@@ -38,12 +38,10 @@ class Register:
                     if role not in user.roles:
                         await self.bot.add_roles(user, role)
                         await self.bot.send_message(user, '{} role has been assigned to you in {}!'.format(role.name, server.name))
-                        await self.bot.delete_message(ctx.message)
                     # --- REMOVE FROM USER ---
                     else:
                         await self.bot.remove_roles(user, role)
                         await self.bot.send_message(user, '{} role has been removed from you in {}!'.format(role.name, server.name))
-                        await self.bot.delete_message(ctx.message)
             except:
                 await self.bot.say('That role isn\'t in this server.')
         else:
@@ -70,7 +68,7 @@ class Register:
             else:
                 msg = "There aren't any roles you can register for in {}".format(server.name)
                 await self.bot.send_message(user, box(msg))
-            await self.bot.delete_message(ctx.message)
+        await self.bot.delete_message(ctx.message)
 
     @commands.group(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
@@ -113,22 +111,17 @@ class Register:
                 await self.bot.say("I cannot create a role. Please assign Manage Roles to me!")
         role = discord.utils.get(server.roles, name=role_name)
         # --- DONE CREATING ROLE! ---
-        # --- JSON SERVER LOGIC ---
-        if server.id not in self.json:
-            log.debug('Adding server({}) in Json'.format(server.id))
-            self.json[server.id] = {}
-            dataIO.save_json(self.location, self.json)
-        # --- DONE JSON SERVER LOGIC! ---
+        add_server_to_json(server)
         # --- ADDING ROLE TO JSON ---
         try:
             if role.id not in self.json[server.id]:
                 # ROLE NOT IN REGISTER
                 self.json[server.id][role.id] = {'role_name': role.name}
                 dataIO.save_json(self.location, self.json)
-                await self.bot.say('``{}`` is now a valid register role.'.format(role.name))
+                await self.bot.say('``{}`` is now in register.'.format(role.name))
             else:
                 # ROLE ALREADY IN REGISTER
-                await self.bot.say('``{}`` is already a valid register role!'.format(role.name))
+                await self.bot.say('``{}`` is already in register!'.format(role.name))
         except:
             await self.bot.say('Something went wrong.')
 
@@ -136,16 +129,45 @@ class Register:
     async def _regedit_removerole(self, ctx, *, role_name: str):
         """Removes a register role."""
         server = ctx.message.server
-        try:
+        if server.id in self.json:
             role = discord.utils.get(server.roles, name=role_name)
-            if role.id in self.json[server.id]:
-                del self.json[ctx.message.server.id][role.id]
+            if role:
+                # ROLE IS IN SERVER
+                if role.id in self.json[server.id]:
+                    # REMOVE ROLE FROM JSON
+                    del self.json[server.id][role.id]
+                    dataIO.save_json(self.location, self.json)
+                    await self.bot.say('``{}`` role has been removed from register.'.format(role.name))
+                else:
+                    # ROLE ISN'T IN JSON
+                    await self.bot.say('``{}`` role isn\'t in register yet.'.format(role.name))
+            else:
+                # ROLE ISN'T IN SERVER
+                await self.bot.say('That role isn\'t in this server.')
+                if role_name in [r['role_name'] for r in self.json[server.id]]:
+                    # TODO: REMOVE ROLE FROM JSON
+                    del self.json[server.id][r]
+                    dataIO.save_json(self.location, self.json)
+                    await self.bot.say('Old role removed from register.')
+                else:
+                    await self.bot.say('That role isn\'t in this server.')
+        else:
+            msg = 'There aren\'t any roles you can register for in this server.'.format(server.name)
+            await self.bot.say(box(msg))
+
+    # TODO : FINISH DELCMDS
+    '''@regedit.command(name="delcmds", pass_context=True, no_pm=True)
+    async def _regedit_delcmds(self, ctx):
+        """Toggles whether or not a the !register command is deleted after being sent."""
+        add_server_to_json(server)'''
+        
+    def add_server_to_json(server):
+        if server.id not in self.json:
+                log.debug('Adding server({}) in Json'.format(server.id))
+                self.json[server.id] = {}
                 dataIO.save_json(self.location, self.json)
-                await self.bot.say('``{}`` is no longer a valid register role.'.format(role.name))
-        except:
-            await self.bot.say('That role isn\'t in this server.')
 
-
+        
 def check_folder():
     if not os.path.exists('data/register'):
         log.debug('Creating folder: data/register')
