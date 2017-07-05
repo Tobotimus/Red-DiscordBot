def2bbedaacfd9f7e12d08e3399145f72ea7fe3e
No Reply <kspree101@gmail.com>
2017-07-05 19:43:35 +0930

    changed permissions for streamalert command

--- cogs/streams.py
+++ cogs/streams.py
@@ -123,7 +123,7 @@ class Streams:
             await self.bot.say(embed=embed)
 
     @commands.group(pass_context=True, no_pm=True)
-    @checks.mod_or_permissions(manage_server=True)
+    @checks.mod_or_permissions(kick_members=True)
     async def streamalert(self, ctx):
         """Adds/removes stream alerts from the current channel"""
         if ctx.invoked_subcommand is None:
